d6c4b0aeb834142169ceef3e22b176e8be8d25a0
Ishan Goswami <ishangoswami315@gmail.com>
2025-04-07 22:08:15 +0530

    new npm package

--- src/index.ts
+++ src/index.ts
@@ -72,7 +72,7 @@ class ExaServer {
   constructor() {
     this.server = new McpServer({
       name: "exa-search-server",
-      version: "0.3.4"
+      version: "0.3.5"
     });
     
     log("Server initialized");
